ca171327aac7d3387af60dcbffa41b6143400bc8
조정현[커머스ID개발] <jeong_hyun.jo@navercorp.com>
2023-11-03 22:47:24 +0900

    명령어 테스트

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
 from config import TOKEN
 import discord
+import time
+from discord.ext import commands
 
-intents = discord.Intents.default()
-intents.message_content = True
 
-client = discord.Client(intents=intents)
+intents = discord.Intents.all()
+intents.members = True
 
-@client.event
+app = commands.Bot(command_prefix='!', intents=intents)
+
+
+def is_me(m):
+    return m.author == client.user
+
+@app.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    await app.change_presence(status=discord.Status.online, activity=None)
 
-@client.event
-async def on_message(message):
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+@app.command()
+async def hello(ctx):
+    await ctx.send("야 조정현 너 친구한테 그러지마!")
 
-client.run(TOKEN)
+@app.command()
+async def 롤체(ctx):
+    await ctx.send("https://lolchess.gg/meta")
+
+@app.command()
+async def 강동혁(ctx):
+    await ctx.send("롤체 개못함")
+
+@app.command()
+async def 청소(ctx):
+    time.sleep(0.5) 
+    await ctx.channel.purge()
+
+app.run(TOKEN)
