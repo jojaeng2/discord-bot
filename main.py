e12a5084f4d4f575241d0b457d6190e21ae9cf83
조정현[커머스ID개발] <jeong_hyun.jo@navercorp.com>
2023-11-03 21:14:57 +0900

    bot init

---
+++ main.py
@@ -0,0 +1,18 @@
+from config import TOKEN
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(TOKEN)
